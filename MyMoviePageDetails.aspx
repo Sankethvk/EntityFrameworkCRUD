41b4c5e608bcdc8840b0de8de1a85d38298d78dd
SanketV <sankethvivek@gmail.com>
2021-10-12 18:05:58 +0530

    CRUD operations using Entity Framework

---
+++ MyMoviePageDetails.aspx
@@ -0,0 +1,56 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="MyMoviePageDetails.aspx.cs" Inherits="DataManagementMovie.MyMoviePageDetails" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <h2>Movie Details</h2>
+            <hr />
+            <table>
+                <tr>
+                    <td>Movie Name</td><td>
+                    <asp:TextBox ID="txtMoviename" runat="server"></asp:TextBox>
+                    </td>
+                </tr>
+                <tr>
+                    <td>Movie Type</td><td>
+                    <asp:TextBox ID="txtMovietype" runat="server"></asp:TextBox>
+                    </td>
+                </tr>
+                <tr>
+                    <td>Movie Description</td><td>
+                    <asp:TextBox ID="txtMoviedesc" runat="server"></asp:TextBox>
+                    </td>
+                </tr>
+                <tr>
+                    <td></td><td>
+                    <asp:Button ID="btnsave" runat="server" Text="Save" OnClick="btnsave_Click" />
+                        <asp:Button ID="btnUpdate" runat="server" Text="Update" OnClick="btnUpdate_Click" />
+                        <asp:Button ID="btnDelete" runat="server" Text="Delete" OnClick="btnDelete_Click" />
+                    <asp:Button ID="btnReset" runat="server" Text="Reset" />
+                    </td>
+
+                </tr>
+                <tr>
+                    <td colspan="2">
+                        <asp:Label ID="lblResult" runat="server" Text=""></asp:Label>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        Movie Id
+                    </td>
+                    <td>
+                        <asp:TextBox ID="txtEditMovieID" runat="server"></asp:TextBox>
+                        <asp:Button ID="btnEdit" runat="server" Text="Edit" OnClick="btnEdit_Click" /></td>
+                </tr>
+            </table>
+        </div>
+    </form>
+</body>
+</html>
---
+++ MyMoviePageDetails.aspx.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using DataManagementMovie;
+using MyMovieClassLibrary;
+using System.Data.Entity;
+
+namespace DataManagementMovie
+{
+    public partial class MyMoviePageDetails : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnsave_Click(object sender, EventArgs e)
+        {
+            MoviedataEntities moviedataentities = new MoviedataEntities();
+            movie movieObj = new movie();
+            movieObj.Name = txtMoviename.Text;
+            movieObj.MovieType = txtMovietype.Text;
+            movieObj.MovieDesc = txtMoviedesc.Text;
+            moviedataentities.movies.Add(movieObj);
+            moviedataentities.SaveChanges();
+            lblResult.Text = "movie is added succesfully !!";
+            
+        }
+
+        protected void btnEdit_Click(object sender, EventArgs e)
+        {
+            MoviedataEntities moviedataentities = new MoviedataEntities();
+            var result = moviedataentities.movies.ToList().Find(obj => obj.Id == Convert.ToInt32(txtEditMovieID.Text));
+            txtMoviename.Text = result.Name;
+            txtMovietype.Text = result.MovieType;
+            txtMoviedesc.Text = result.MovieDesc;
+        }
+
+        protected void btnUpdate_Click(object sender, EventArgs e)
+        {
+            MoviedataEntities moviedataEntities = new MoviedataEntities();
+            movie movieObj = new movie();
+            movieObj.Id = Convert.ToInt32(txtEditMovieID.Text);
+            movieObj.Name = txtMoviename.Text;
+            movieObj.MovieType = txtMovietype.Text;
+            movieObj.MovieDesc = txtMoviedesc.Text;
+            moviedataEntities.Entry(movieObj).State = EntityState.Modified;
+            moviedataEntities.SaveChanges();
+            lblResult.Text = "Movie updated successfully!!! ";
+
+        }
+
+        protected void btnDelete_Click(object sender, EventArgs e)
+        {
+            MoviedataEntities moviedataEntities = new MoviedataEntities();
+            movie movieObj = new movie();
+            movieObj.Id = Convert.ToInt32(txtEditMovieID.Text);
+            movieObj.Name = txtMoviename.Text;
+            movieObj.MovieType = txtMovietype.Text;
+            movieObj.MovieDesc = txtMoviedesc.Text;
+            moviedataEntities.Entry(movieObj).State = EntityState.Deleted;
+            moviedataEntities.SaveChanges();
+            lblResult.Text = "movie is deleted successfully!!";
+
+        }
+    }
+}
